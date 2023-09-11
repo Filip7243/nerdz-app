2464f25554b54addb1a43a9e15b8154e6ad64b58
Filip7243 <filip7243@gmail.com>
2023-09-11 19:25:40 +0200

    init

---
+++ src/main/java/com/nerdz/nerdzapp/NerdzAppApplication.java
@@ -0,0 +1,13 @@
+package com.nerdz.nerdzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdzAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerdz/nerdzapp/NerdzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdz.nerdzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
